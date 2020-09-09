4cc19c3b21344d5a36309858431f8430275e4b4f
Totiruzi <totiruzi@yahoo.com>
2020-09-09 17:18:20 +0300

    Add route and header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -34,28 +36,29 @@ const options = [
     value: 'blue',
   },
 ];
+
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  // const [showDropdown, setShowDropdown] = useState(true);
+  const [selected, setSelected] = useState(options[0])
   return (
     <div>
-      {/* <button
-        onClick={() => {
-          setShowDropdown(!showDropdown);
-        }}
-      >
-        Toggle Dropdown
-      </button> */}
-      {/* <Accordion items={items}/> */}
-      {/* <Search /> */}
-      {/* {showDropdown ? (
+      <Header />
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
         <Dropdown
+          label="Select a Color"
+          options= {options}
           selected={selected}
           onSelectedChange={setSelected}
-          options={options}
         />
-      ) : null} */}
-      <Translate />
+      </Route>
+      <Route path='/translate'>
+        <Translate/>
+      </Route>
     </div>
   );
 };
